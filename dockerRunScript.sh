1f271e88af8baf4b5f279aef214238b0407b8658
NeelParihar <neelparihar599@gmail.com>
2021-02-12 22:28:16 +0530

    added run script for docker

---
+++ dockerRunScript.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+
+# git clone the repo
+
+# cd to the cloned repo directory
+
+
+# Create the container image, this will use the Dockerfile
+
+docker build -t xmeme_app .
+
+# Run the app container on port 8081
+
+docker run -d --net="host" xmeme_app
+
+# Run sleep.sh
+
+chmod +x sleep.sh
+
+./sleep.sh
+
+
+# Execute the POST /memes endpoint using curl
+
+curl --location --request POST 'http://<Server_URL>/memes' \
+
+--header 'Content-Type: application/json' \
+
+--data-raw '{
+
+"name": "xyz",
+
+"url": "abc.com",
+
+"caption": "This is a meme"
+
+}'
+
+
+# Execute the GET /memes endpoint using curl
+
+curl --location --request GET 'http://localhost:8081/memes'
