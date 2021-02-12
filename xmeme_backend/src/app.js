c43427baaea2244384700616b72c6b699fc2b949
NeelParihar <neelparihar599@gmail.com>
2021-02-12 12:08:55 +0530

    removed not required dependencies

--- xmeme_backend/src/app.js
+++ xmeme_backend/src/app.js
@@ -5,18 +5,15 @@ const mongoSanitize = require('express-mongo-sanitize');
 const compression = require('compression');
 const cors = require('cors');
 const httpStatus = require('http-status');
-const config = require('./config/config');
 const morgan = require('./config/morgan');
-const routes = require('./routes/v1');
+const routes = require('./routes');
 const { errorConverter, errorHandler } = require('./middlewares/error');
 const ApiError = require('./utils/ApiError');
 
 const app = express();
 
-if (config.env !== 'test') {
-  app.use(morgan.successHandler);
-  app.use(morgan.errorHandler);
-}
+app.use(morgan.successHandler);
+app.use(morgan.errorHandler);
 
 // set security HTTP headers
 app.use(helmet());
