ec36e2bb334ecc623cd0777904aecadc48c8b4ac
NeelParihar <neelparihar599@gmail.com>
2021-02-13 13:21:20 +0530

    ui fixes

--- xmeme_frontend/src/components/theme/Header/NavbarLinks/index.jsx
+++ xmeme_frontend/src/components/theme/Header/NavbarLinks/index.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { ThemeContext } from "providers/ThemeProvider";
 import ToggleTheme from "components/theme/Header/ToggleTheme";
 import { Wrapper } from "./styles";
+import config from "data/neelparihar"
 
 const NavbarLinks = ({ desktop }) => {
   const { theme } = useContext(ThemeContext);
@@ -9,6 +10,8 @@ const NavbarLinks = ({ desktop }) => {
   return (
     <Wrapper desktop={desktop} theme={theme}>
       <div className="menu">
+        <a  target="_blank" rel="noreferrer" href={config.githubUrl}>My Github</a>
+        <a  target="_blank" rel="noreferrer" href={config.linkedinUrl}>My LinkedIn</a>
         <ToggleTheme />
       </div>
     </Wrapper>
